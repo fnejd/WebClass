5dad3ea57d6ef95104cd72be9f1e704a0ec101df
fnejd <97046334+fnejd@users.noreply.github.com>
2022-01-13 23:44:24 +0900

    Add files via upload

---
+++ JAVA_BAEKJOON/1_IO/num_10869.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class num_10869 {
+	public static void main(String[] args) {
+		
+		
+		//사칙연산
+		
+		//두 자연수 A와 B
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		//A+B
+		System.out.println(a+b);
+		
+		//A-B
+		System.out.println(a-b);
+		
+		//A*B
+		System.out.println(a*b);
+		
+		//A/B
+		System.out.println(a/b);
+		
+		//A%B
+		System.out.println(a%b);
+		
+		
+		
+	}
+
+}
