a6b0bdaf7f99ceb4cd50afa011ceccf9867be93a
fnejd <97046334+fnejd@users.noreply.github.com>
2022-01-13 23:35:41 +0900

    Add files via upload

---
+++ JAVA_BAEKJOON/1_IO/num_10171.java
@@ -0,0 +1,15 @@
+
+public class num_10171 {
+
+	public static void main(String args[]) {
+		
+		//고양이
+		
+		//고양이 출력
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+}
