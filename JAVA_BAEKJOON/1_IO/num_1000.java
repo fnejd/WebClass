52256c26ff530fd450a754f4de56e322c66205fc
fnejd <97046334+fnejd@users.noreply.github.com>
2022-01-13 23:39:20 +0900

    Add files via upload

---
+++ JAVA_BAEKJOON/1_IO/num_1000.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class num_1000 {
+	public static void main(String[] args) {
+		
+		//A+B
+		
+		//정수 A와 B를 입력받고 A+B를 출력
+		
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a+b);
+		
+		sc.close();
+		
+	}
+
+}
