8ec0cc61628f040de9f5a27e7f57face934750c3
fnejd <97046334+fnejd@users.noreply.github.com>
2022-01-13 23:46:03 +0900

    Add files via upload

---
+++ JAVA_BAEKJOON/1_IO/num_10430.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class num_10430 {
+	public static void main(String[] args) {
+		
+		//나머지
+		
+		
+		Scanner sc = new Scanner(System.in);
+		int a,b,c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		//(A+B)%C 출력
+		System.out.println((a + b) %c);
+		
+		//((A%C) + (B%C))%C 출력
+		System.out.println((a%c + b%c)%c);
+		
+		//(A×B)%C 출력
+		System.out.println((a * b) %c);
+		
+		//((A%C) × (B%C))%C 출력
+		System.out.println(((a%c)*(b%c))%c);
+		
+	}
+
+}
