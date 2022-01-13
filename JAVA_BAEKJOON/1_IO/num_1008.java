a3c5581ab6bfdda8025eba44d6333a5a30b88a04
fnejd <97046334+fnejd@users.noreply.github.com>
2022-01-13 23:42:06 +0900

    Add files via upload

---
+++ JAVA_BAEKJOON/1_IO/num_1008.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class num_1008 {
+	public static void main(String[] args) {
+		
+		//A/B
+		
+		//두 정수 A와 B를 입력받고 A/B를 출력
+		//상대오차가 10 ^(-9) 이하
+		
+		Scanner sc = new Scanner(System.in);
+		int a ,b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println((double)a/b);
+	}
+
+}
