d1cfbc08dae432dfa25d337238d050c05dc023dd
fnejd <97046334+fnejd@users.noreply.github.com>
2022-01-13 23:34:24 +0900

    Add files via upload

---
+++ JAVA_BAEKJOON/1_IO/num_10718.java
@@ -0,0 +1,12 @@
+
+public class num_10718{
+	public static void main(String[] args) {
+		
+		//We love kriii
+		//강한친구 대한육군 한 줄에 한번씩 총 두줄 출력
+		
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+
+}
