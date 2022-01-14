24f4289857920055735dd02eeb15e8275531242f
fnejd <97046334+fnejd@users.noreply.github.com>
2022-01-15 01:29:33 +0900

    Add files via upload

---
+++ JAVA_BAEKJOON/2_If/num_14681.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class num_14681 {
+	public static void main(String[] args) {
+		
+		//사분면 고르기
+		
+		Scanner sc = new Scanner(System.in);
+		int x,y;
+		
+		//좌표 입력 받기
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		if(x>0) {
+			//x >0 이면 1 또는 4사분면 
+			if(y>0) {
+				//x>0 && y>0 
+				System.out.println(1);
+			}else {
+				//x>0 && y<0
+				System.out.println(4);
+			}
+		}else {
+			//x <0 이면 2 또는 3사분면 
+			if(y>0) {
+				//x<0 && y>0
+				System.out.println(2);
+			}else {
+				//x<0 && y<0
+				System.out.println(3);
+			}		
+		}
+		
+	}
+
+}
