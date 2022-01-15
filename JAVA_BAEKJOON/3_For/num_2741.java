001172209f261e1b5b45de511554cb2a72bde1ec
fnejd <97046334+fnejd@users.noreply.github.com>
2022-01-16 01:50:07 +0900

    Add files via upload

---
+++ JAVA_BAEKJOON/3_For/num_2741.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class num_2741 {
+	public static void main(String[] args) {
+		
+		//n찍기
+		Scanner sc = new Scanner(System.in);
+		
+		//n 입력 받기
+		int n = sc.nextInt();
+		
+		
+		//1에서 n까지 한 줄에 하나씩 출력
+		for(int i=1; i<= n; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
